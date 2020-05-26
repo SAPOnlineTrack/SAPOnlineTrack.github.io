5254374a0d205f0cc209475d18b43cc867a65c31
katansapdevelop <katan.sap.development@gmail.com>
2020-05-26 22:31:41 +1000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7f1e5230fef3f75e7419.js"
+    "url": "webpack-runtime-88d1e48c66b83945a402.js"
   },
   {
     "url": "commons-c59e864705e4891367e3.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "716254905bc5a2cd5c949118185cc9a1"
+    "revision": "ee3094dc0b4ae98e636898ae7ddca150"
   },
   {
     "url": "manifest.webmanifest",
